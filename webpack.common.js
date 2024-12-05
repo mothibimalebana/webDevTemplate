0396f39c7548ece9fc7274a5de234d77378ac064
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-05 13:34:02 +0200

    webpack is up and running

--- webpack.common.js
+++ webpack.common.js
@@ -3,31 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './src/js/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'App Name',
-      filename: '[name].js',
+      filename: 'index.html',
       template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-};
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Production',
     }),
   ],
   output: {
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,6 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    watchFiles: ['./src/index.html'],
+    watchFiles: './src/index.html',
   },
 });
