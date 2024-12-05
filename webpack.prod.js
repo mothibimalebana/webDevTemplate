5c8101f49fc1840df64b9657d12ea766b34a2807
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-05 21:07:19 +0200

    added css loader and styler loader to webpack.config.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,14 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 });
