8a4acdadd6b7031e9ecdc23806221091e5feac78
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-05 21:08:59 +0200

    added css loader and styler loader to webpack.config.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,29 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: "./src/js/index.js",
+    app: './src/js/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "App Name",
-      filename: "index.html",
-      template: "./src/index.html",
+      title: 'App Name',
+      filename: 'index.html',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,4 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-});
+module.exports = merge(common, {});
